cf22ecb905961a4fb9e661e0d61a48edb0003ead
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2024-07-26 17:32:37 -0500

    secure api key

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -5,8 +5,7 @@ export const API_HEADER = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjA2YmEyMGViZmMzOWIyODc2MjBmNjUwZDljMzE0OCIsIm5iZiI6MTcyMTg0NjE5NC40MTc4ODgsInN1YiI6IjY2YTE0NWU3YWVlZTA3MTZkMDE0YjRjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g2dmLa2-T6PTdw1_WF5ZwEvA-lIPzW_x7oyFzOdY3_8",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 export const MOVIE_VIDEO_URL =
